637c144c7865ec472323496472269b57d16ed43c
hjx422 <hjx422@163.com>
2017-09-30 14:58:57 +0800

    add: 支持路由

--- src/server.js
+++ src/server.js
@@ -5,53 +5,32 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const Views = require('koa-views')
 
-//const app = new Koa()
-//const router = new Router()
-//
-//app.use(Views(__dirname + '/views', {
-//    map: { html: 'mustache' }
-//}))
-//
-//const route = router.get('/', (ctx, next) => {
-//    ctx.render('app.html', {
-//        pageTitle: '哈哈哈'
-//    })
-//})
-//
-//app.use(route)
-//
-//app.listen(3000)
-//console.log('Koa started on port 3000')
-
 const app = new Koa()
 const router = new Router()
 
 app.use(Views(__dirname + '/views', {
     map: {
-        jade: 'jade',
-        html: 'mustache'
+        jade: 'jade',        // jade文件采用jade引擎进行模板解析
+        html: 'mustache'     // html文件采用mustache引擎进行模板解析
     }
 }))
 
-//const indexRoute = router.get('/', async (ctx, next) => {
-//    await ctx.render('app.html', {
-//        pageTitle: '哈哈'
-//    })
-//})
-
 const indexPage = async (ctx, next) => {
     await ctx.render('app.html', {
-        pageTitle: '哈哈=='
+        pageTitle: '首页'
     })
 }
 
-//app.use(async (ctx, next) => {
-//    await ctx.render('app.html', {
-//        pageTitle: '哈哈'
-//    })
-//})
+const consolePage = async (ctx, next) => {
+    await ctx.render('app.html', {
+        pageTitle: '控制台页面'
+    })
+}
 
-app.use(indexPage)
+router.get('/', indexPage)
+router.get('/console', consolePage)
+
+app.use(router.routes())
 
 app.listen(3000)
 console.log('Koa started on port 3000')
